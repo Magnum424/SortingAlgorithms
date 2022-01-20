9e836a453adfd29f29191a95b55c8dfcbd6d0a37
Alejandro Arcucci <ale_arc2015@hotmail.com>
2022-01-20 13:21:10 -0300

    Selection sort working

---
+++ SelectionSort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Execution time O(n^2) - O(n) if it's already sorted.
+//Declare a template to make the algorithm generic
+template <class T>
+//The algorithm takes the reference of a vector of any size. Selection sort is not recomended for long vectors.
+void selectionSort(vector<T>& v){
+	for(int i = 0; i < v.size() - 1; ++i){
+		int min = i; //Minimum value
+		for(int j = i + 1; j < v.size(); ++j){
+			if(v[min] > v[j])//Checks the values of the vector on i and i+1
+				min = j;
+		}
+		//Swaps the position of the values.
+		T aux = v[i];
+		v[i] = v[min];
+		v[min] = aux;
+	}
+};
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "SelectionSort.cpp"
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main(int argc, char** argv) {
+	srand(time(NULL));
+	vector<int> v;
+	for(int i = 0; i < 10; i++){
+		v.push_back(rand() % 25);
+	}
+	for(int i = 0; i < v.size(); i++){
+		cout << v[i] << " ";
+	}
+	cout << "\n";	
+	selectionSort(v);
+	for(int i = 0; i < v.size(); i++){
+		cout << v[i] << " ";
+	}
+	return 0;
+}
